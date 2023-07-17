9ca2552e420ae2629fc419111c8dcaffe205256a
Claudio Gallardo M <claudio.gallardo@externos.sura.cl>
2023-07-17 18:39:29 -0400

    Fin de seccion 4

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent {
+
+  // public heroName:string= ''
+  public name:string = 'ironman';
+  public age:number = 45;
+
+  get capitalizedName():string{
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription():string{
+    return `${this.name} - ${this.age}`;
+  }
+
+  changeHero():void{
+
+    this.name = 'the Punisher';
+
+  }
+
+  changeAge():void{
+    this.age = 37;
+  }
+
+  resetAll():void{
+    this.name ='ironman';
+    this.age = 45;
+  }
+
+}
